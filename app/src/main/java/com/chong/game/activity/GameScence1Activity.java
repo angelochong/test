2481ef088f2b472b207b68144e06295b9878aec2
chenglong <chenglong@renrendai.com>
2015-07-17 15:54:25 +0800

    on develop test15

--- app/src/main/java/com/chong/game/activity/GameScence1Activity.java
+++ app/src/main/java/com/chong/game/activity/GameScence1Activity.java
@@ -38,6 +38,7 @@ public class GameScence1Activity extends BaseActivity{
     //on develop test12
     //on develop test13
     //on develop test14
+    //on develop test15
 	private View loginButton;
 	private View registerButton;
 	private View usersButton;
