c33ee4334972ee5398ab04dd0762d3c4762905c7
chenglong <chenglong@renrendai.com>
2015-07-17 14:23:09 +0800

    test3

--- app/src/main/java/com/chong/game/activity/GameScence1Activity.java
+++ app/src/main/java/com/chong/game/activity/GameScence1Activity.java
@@ -14,6 +14,7 @@ import com.chong.game.vo.RequestVo;
 public class GameScence1Activity extends BaseActivity{
 //test
 	//test
+    //test
 	private View loginButton;
 	private View registerButton;
 	private View usersButton;
